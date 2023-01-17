f5851a4c9ff6c84e80f434217333ab6b7d67221f
rostiq <rostiknatkha@gmail.com>
2023-01-17 15:32:49 +0200

    create new project
    npm install,
    add title and description

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import play from "./play.png";
+
 export const App = () => {
   return (
     <div
@@ -7,10 +9,41 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <h1>
+        Movielist
+      </h1>
+      <img src={play} alt="play" width="160"/>
+      <p>
+        easy and fast app for creating movie's playlists
+      </p>
     </div>
   );
 };
+
+//how insert image to img?
+// {}
+
+// <img src={"./logo.jpeg"} />
+
+// <img src={require('./logo.jpeg')} />
+
+// import logo from './logo.jpeg'; // with import
+
+// const logo = require('./logo.jpeg); // with require
+
+// <img src={logo} />
+
+// .
+
+// <img src={require('./one.jpeg')} />
+
+// <img src={require('./logo.jpeg').default} />
+
+
+
+//Source: https://stackoverflow.com/questions/39999367
+
+
