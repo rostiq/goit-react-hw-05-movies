539bc98d8be1dcd69e0441299463116da5c50c53
rostiq <rostiknatkha@gmail.com>
2023-01-25 20:52:34 +0200

    just playing

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -32,45 +32,45 @@ const MovieDetails = () => {
     });
 
     return (
-        <Wrapper>
-            <GetBackBtn to={backLinkHref}>get back</GetBackBtn>
-            {movieData && <div style={{
-                display: 'flex',
-                gap: '1rem',
-                justifyContent: "center",
-                alignItems: "flex-start",
-                marginTop: "1rem"
-            }}>
-                <div style={{
+            <Wrapper>
+                <GetBackBtn to={backLinkHref}>get back</GetBackBtn>
+                {movieData && <div style={{
                     display: 'flex',
-                    flexShrink: 0,
-                    width: '25vw'
+                    gap: '1rem',
+                    justifyContent: "center",
+                    alignItems: "flex-start",
+                    marginTop: "1rem"
                 }}>
-                    <img src={poster_path
-                        ? `https://image.tmdb.org/t/p/w500${poster_path}`
-                        : noimage} alt={title} />
-                </div>
-                <div>
-                    <div>
-                        <span>{title}</span>
-                        <p>release: {formattedDate}</p>
-                        <p>rate: {vote_average}</p>
-                        <p>{overview || 'No desrition yet'}</p>
-                    </div>
-
                     <div style={{
                         display: 'flex',
-                        gap: '4vw',
+                        flexShrink: 0,
+                        width: '25vw'
                     }}>
-                        <Styled to="cast">cast</Styled>
-                        <Styled to="review">review</Styled>
+                        <img src={poster_path
+                            ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                            : noimage} alt={title} />
                     </div>
-                    <Suspense fallback={<p>loading...</p>}>
-                        <Outlet />
-                    </Suspense>
-                </div>
-            </div>}
-        </Wrapper>
+                    <div>
+                        <div>
+                            <span>{title}</span>
+                            <p>release: {formattedDate}</p>
+                            <p>rate: {vote_average}</p>
+                            <p>{overview || 'No desrition yet'}</p>
+                        </div>
+
+                        <div style={{
+                            display: 'flex',
+                            gap: '4vw',
+                        }}>
+                            <Styled to="cast">cast</Styled>
+                            <Styled to="review">review</Styled>
+                        </div>
+                        <Suspense fallback={<p>loading...</p>}>
+                            <Outlet />
+                        </Suspense>
+                    </div>
+                </div>}
+            </Wrapper>
     )
 }
 
